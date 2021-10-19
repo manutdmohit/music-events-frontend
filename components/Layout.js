09d4e0ca0450d573cb6228beff25a28da386ff38
manutdmohit <saudmohit@gmail.com>
2021-10-19 05:54:37 +0545

    head tags and layout. also added pages

---
+++ components/Layout.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import styles from '../styles/Layout.module.css';
+
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+      </Head>
+      <div className={styles.container}>{children}</div>
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: 'Music Events | Find the hottest parties and concerts',
+  description: 'Find the latest and upcoming musical events',
+  keywords: 'music,dj,edm,hiphop,concert,guitar,events',
+};
