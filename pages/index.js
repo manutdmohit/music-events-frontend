1ee86f941dff64157a93725e417b3e8be96734ce
manutdmohit <saudmohit@gmail.com>
2021-10-19 20:29:56 +0545

    get server side props and get static side props

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function HomePage() {
+// export const getServerSideProps = async () => {
+//   const res = await fetch(`${API_URL}/api/events`);
+//   const events = await res.json();
+
+//   return {
+//     props: { events },
+//   };
+// };
+export const getStaticProps = async () => {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
+};
+
+export default function HomePage({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <h3 key={evt.id}>{evt.name}</h3>
+      ))}
     </Layout>
   );
 }
