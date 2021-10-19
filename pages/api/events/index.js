d14c1921b2ee887cc4f0115f7337c4c275c166ca
manutdmohit <saudmohit@gmail.com>
2021-10-19 12:04:29 +0545

    creating api routes

--- pages/api/events/index.js
+++ pages/api/events/index.js
@@ -1,5 +1,10 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { events } = require('./data.json');
 
-export default (req, res)=> {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default (req, res) => {
+  if (req.method === 'GET') {
+    res.status(200).json(events);
+  } else {
+    res.setHeader('Allow', ['Get']);
+    res.status(405).json({ message: `method ${req.method} is not allowed` });
+  }
+};
